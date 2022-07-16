33547c30b21f0d61199fcb59b8607da8a9ac8489
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-16 17:03:29 +0500

    001_значение координаты по номеру четверти

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите четверть");
+int chet = Int32.Parse(System.Console.WriteLine());
+if(chet == 1)
+{
+    System.Console.WriteLine("x>0 и y>0");
+}
+else if(chet == 2)
+{
+    System.Console.WriteLine("x<0 и y>0");
+}
+else if(chet == 3)
+{
+    System.Console.WriteLine("x<0 и y<0");
+}
+else if(chet == 4)
+{
+    System.Console.WriteLine("x>0 и y<0");
+}
+else
+System.Console.WriteLine("Введите четверть коректно");
