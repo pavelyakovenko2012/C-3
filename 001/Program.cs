82c96789e2e7e6770c363c0bf397b0cceaaa13df
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-07-16 17:01:06 +0500

    001_Номер четверти координаты

---
+++ 001/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите координату X:");
+int x = Int32.Parse(System.Console.WriteLine());
+Console.Write("Введите координату Y:");
+int y = Convert.ToInt32(System.Console.WriteLine());
+if(x>0 && y>0)
+{
+    System.Console.WriteLine("Четверть: 1");
+}
+else if(x<0 && y>0)
+{
+    System.Console.WriteLine("Четверть: 2");
+}
+else if(x<0 && y<0)
+{
+    System.Console.WriteLine("Четверть: 3");
+}
+else if(x>0 && y<0)
+{
+    System.Console.WriteLine("Четверть: 4");
+}
+else
+System.Console.WriteLine("Введите коректно координаты");
